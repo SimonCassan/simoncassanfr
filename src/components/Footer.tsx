4206877886770c6b447e4886365162ea07a0f393
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-07-04 15:06:39 +0200

    Orange 😍

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,5 +1,5 @@
 function Footer() {
-    return <footer className="p-2 tracking-wider">© {new Date().getFullYear()} Simon Cassan - Site créé avec React + Tailwind via <a href="https://vite.dev/" target="_blank">Vite</a></footer>
+    return <footer className="px-8 py-3 tracking-wider border-t-2 [border-top-color:#f77820]">© {new Date().getFullYear()} Simon Cassan - Site créé avec React + Tailwind via <a href="https://vite.dev/" target="_blank">Vite</a></footer>
 }
 
 export default Footer;
