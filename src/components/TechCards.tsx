33a74f8be9c5fdaa185845be81f640a97b7b5721
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-07-06 17:25:50 +0200

    Add light mode

--- src/components/TechCards.tsx
+++ src/components/TechCards.tsx
@@ -12,7 +12,7 @@ export default function TechCards({ techs }: TechCardsProps) {
                     key={tech.name}
                     className={`flex flex-col items-center justify-center ${tech.bg} rounded-2xl p-4  w-[150px] hover:-translate-y-2 shadow-[0_4px_24px_0_rgba(255,255,255,0.1)] hover:shadow-[0_8px_32px_0_rgba(255,255,255,0.2)] hover:scale-105 transition-all duration-200`}>
                     <span aria-hidden="true" className={`text-4xl mb-2 ${tech.iconColor}`}>{tech.icon}</span>
-                    <span className="font-medium text-sm">{tech.name}</span>
+                    <span className="font-medium text-sm text-white">{tech.name}</span>
                 </div>
             ))}
         </div>
