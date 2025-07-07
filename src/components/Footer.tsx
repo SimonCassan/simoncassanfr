7ba5e96b65e098dbb2b0ef8addf8caf5facfd3b3
SimonCassan <167959390+SimonCassan@users.noreply.github.com>
2025-07-07 08:40:47 +0200

    Improve light mode

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -1,7 +1,7 @@
 function Footer() {
     return (
         <footer className="my-5 tracking-wider">
-            <div className="w-[40%] m-auto mb-5 border-t-2 [border-top-color:#780000] dark:[border-top-color:#f77820]"></div>
+            <div className="w-[40%] m-auto mb-5 border-t-1 [border-top-color:#780000] dark:border-t-2 dark:[border-top-color:#f77820]"></div>
             <p>©&nbsp;{new Date().getFullYear()} Simon&nbsp;Cassan - Site créé avec React + Tailwind via <a href="https://vite.dev/" target="_blank">Vite</a></p>
         </footer>
     )
